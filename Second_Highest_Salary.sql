eee77b158c8b87f132e10e1d753f344edfde913d
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-05-23 21:28:08 +0530

    Update Second_Highest_Salary.sql

--- Second_Highest_Salary.sql
+++ Second_Highest_Salary.sql
@@ -1,12 +1,12 @@
 -- Write a query to find the second highest salary in an employee table.
 
--- USE interview;
+USE interview;
 
--- CREATE TABLE employee ( 
---     emp_id INT PRIMARY KEY,
---     emp_name VARCHAR(50),
---     salary INT 
--- );
+CREATE TABLE employee ( 
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    salary INT 
+);
 
 
 select emp_name,salary from employee
