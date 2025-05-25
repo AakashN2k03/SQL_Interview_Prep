02d74fb2b579ca4d0c27fa41b984c9f7a34a9b31
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-05-25 13:52:56 +0530

    Create 8.Employees share same salary.sql

---
+++ 8.Employees share same salary.sql
@@ -0,0 +1,18 @@
+-- Use the database
+USE interview;
+
+CREATE TABLE employee_5 (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    salary INT
+);
+
+-- Write a query to count how many employees share the same salary.
+select salary,count(*)
+from employee_5
+group by salary
+
+ if name also needed
+select group_concat(name) as name,salary,count(*)
+from employee_5
+group by salary
