07016bd5bdde7904292fd1b7f86f5b885c368a9c
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-05-27 14:22:59 +0530

    Create 15.Delete duplicate rows in a table without using the ROWID keyword.sql

---
+++ 15.Delete duplicate rows in a table without using the ROWID keyword.sql
@@ -0,0 +1,27 @@
+-- Use the database
+USE interview;
+
+CCREATE TABLE employee_7 (
+    EmployeeID INT,
+    Name VARCHAR(50),
+    Department VARCHAR(50)
+);
+
+-- to remove safe mode
+SET SQL_SAFE_UPDATES = 0; 
+
+-- query to delete duplicate rows in a table without using the ROWID keyword.
+
+DELETE FROM employee_7
+WHERE EmployeeID NOT IN (
+    SELECT * FROM (
+        SELECT MIN(EmployeeID)
+        FROM employee_7
+        GROUP BY Name, Department
+    ) AS temp
+);
+
+select * from employee_7;
+
+-- enable safe mode
+SET SQL_SAFE_UPDATES = 0; 
