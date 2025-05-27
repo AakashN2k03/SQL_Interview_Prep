bb83d4ea12db9cd97094dceb5bc52ab0c40303d9
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-05-27 14:42:05 +0530

    Create 16.Records updated within the last hour.sql

---
+++ 16.Records updated within the last hour.sql
@@ -0,0 +1,21 @@
+-- Use the database
+USE interview;
+
+CREATE TABLE employee_9 (
+    EmployeeID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Salary INT,
+    updated_at DATETIME
+);
+
+INSERT INTO employee_9 (EmployeeID, Name, Salary, updated_at) VALUES
+(1, 'Alice', 80000, NOW() - INTERVAL 30 MINUTE),
+(2, 'Bob', 70000, NOW() - INTERVAL 90 MINUTE),
+(3, 'Carol', 95000, NOW() - INTERVAL 10 MINUTE),
+(4, 'David', 85000, NOW() - INTERVAL 2 HOUR);
+
+-- query to fetch records updated within the last hour.
+ 
+select *
+from employee_9
+where updated_at >=now()- interval 1 hour;
